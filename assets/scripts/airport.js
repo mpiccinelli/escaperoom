7631500eb1744986b43da595fafdcdf3af5eaaa6
mpiccinelli <mpicinelli@opiamarketing.cl>
2023-08-24 10:33:34 -0300

    Upda

--- assets/scripts/airport.js
+++ assets/scripts/airport.js
@@ -1,3 +1,7 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
+/// <reference path="../../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 console.log("Open Map Airport");
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+   console.log('Scripting API ready');
+   console.log('Player tags: ',WA.player.tags);
+}).catch(e => console.error(e));
