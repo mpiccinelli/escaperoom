b595bc698758b0a0fd0e1eeae8489591823f1d65
mpiccinelli <mpicinelli@opiamarketing.cl>
2023-08-24 18:16:08 -0300

    Upda

--- assets/scripts/A001.js
+++ assets/scripts/A001.js
@@ -4,40 +4,34 @@ import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
 console.log("Open Map A001");
 // Waiting for the API to be ready
 
-let currentPopup = undefined;
-
 WA.onInit().then(() => {
    console.log('Scripting API ready');
    console.log('Player tags: ', WA.player.tags);
 }).catch(e => console.error(e));
 
 // Popup 1
+let popup1 = undefined;
 WA.room.onEnterLayer('popup_zona').subscribe(() => {
-   infoPopup = WA.ui.openPopup("popup_info", "Desculpe o incoveniente, estamos tentando resolver a pane dos nossos sistemas. Enquanto isso, se precisar de algo, temos um totem aqui do lado onde você pode buscar todas as informações que precisa!", [{
-      label: "Precisa de ajuda?",
+   popup1 = WA.ui.openPopup("popup_info", "Desculpe o incoveniente, estamos tentando resolver a pane dos nossos sistemas. Enquanto isso, se precisar de algo, temos um totem aqui do lado onde você pode buscar todas as informações que precisa!", [{
+      label: "Obrigado!",
       className: "primary",
       callback: (popup) => {
          popup.close();
       }
    }]);
 });
-WA.room.onLeaveZone('popup_zona2', closePopUp);
+WA.room.onLeaveZone('popup_zona2', popup1.close());
 
 // Popup 2
+let popup2 = undefined;
 WA.room.onEnterLayer('popup_zona').subscribe(() => {
-   currentPopup = WA.ui.openPopup("popup_info2", "Ao pousar tivemos uma pequena pane em nossos sistemas e a porta do avião não esta abrindo manualmente. Precisamos do código de desbloqueio para resetar os sistemas e liberar a saída de todos. Pode me ajudar a encontrar?", [{
-      label: "Desembarque...",
+   popup2 = WA.ui.openPopup("popup_info2", "Ao pousar tivemos uma pequena pane em nossos sistemas e a porta do avião não esta abrindo manualmente. Precisamos do código de desbloqueio para resetar os sistemas e liberar a saída de todos. Pode me ajudar a encontrar?", [{
+      label: "Vamos lá!",
       className: "primary",
       callback: (popup) => {
          popup.close();
       }
    }]);
 });
-WA.room.onLeaveZone('popup_zona2', closePopUp);
+WA.room.onLeaveZone('popup_zona2', popup2.close());
 
-function closePopUp() {
-   if (currentPopup !== undefined) {
-      currentPopup.close();
-      currentPopup = undefined;
-   }
-}
