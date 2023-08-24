6bc4b9ada73c066abdb8c00070039a39b5fa55db
mpiccinelli <mpicinelli@opiamarketing.cl>
2023-08-24 10:25:30 -0300

    Update A001.js

--- assets/scripts/A001.js
+++ assets/scripts/A001.js
@@ -12,21 +12,3 @@ WA.onInit().then(() => {
    }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-let currentPopup = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
-
-WA.room.onEnterZone('clock', () => {
-    WA.chat.sendChatMessage("Message from the Scripting API", "MrRobot");
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveZone('clock', closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
