de2108ada37b969048ac6e39fed8c409f50798a3
mpiccinelli <mpicinelli@opiamarketing.cl>
2023-08-24 10:29:53 -0300

    UPDATE

--- assets/scripts/A001.js
+++ assets/scripts/A001.js
@@ -1,14 +1,7 @@
 /// <reference path="../../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 console.log("Open Map A001");
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
    console.log('Scripting API ready');
-   console.log('Player tags: ',WA.player.tags)
-
-   // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-   bootstrapExtra().then(() => {
-       console.log('Scripting API Extra ready');
-   }).catch(e => console.error(e));
-
+   console.log('Player tags: ',WA.player.tags);
 }).catch(e => console.error(e));
